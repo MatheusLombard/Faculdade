ffc7265c497d03f63ba550758e85ad18b135ddaf
MATHEUS LOMBARDI DE SOUZA <26126@unifacef.edu.br>
2026-05-12 20:43:35 -0300

    'banco'

---
+++ 2°Bimestre/Paradigmas da Programação/db/src/main/java/dc/unifacef/db/model/Produto.java
@@ -0,0 +1,19 @@
+package dc.unifacef.db.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String nome;
+    @Column (nullable = false)
+    private Double preco;
+    @Column (length = 500)
+    private String descricao;
+
+}
